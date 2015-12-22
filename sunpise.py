cb2110325802d6425ded031dce626fda0ac56bbf
Caleb Gross <caleb.gross@gmail.com>
2015-12-21 21:43:28 -1000

    Update print() statements for Python 3.x

--- sunpise.py
+++ sunpise.py
@@ -22,8 +22,8 @@ from time import sleep
 
 # get Charlottesville sunrise time from the web
 def getSunrise():
-	url = "http://www.gaisma.com/en/location/charlottesville-virginia.html"
-	r = requests.get(url)
+	url = "http://www.timeanddate.com/astronomy/usa/kailua-kona"
+	r = requests.get(url).text
 	data = r.text
 	soup = BeautifulSoup(data)
 	tag = soup.find("td", {"class" : "dawn"})
@@ -37,14 +37,14 @@ def wait_start(sunTimes, still_interval):
 	startTime = time(*(map(int, sunTimes[0].split(':'))))
 	now = datetime.today().time()
 	while startTime > now:
-		print now.strftime("%H:%M") + " not yet " + startTime.strftime("%H:%M")
+		print(now.strftime("%H:%M") + " not yet " + startTime.strftime("%H:%M"))
 		sleep(60)					# wait for 60 seconds before trying again
 		now = datetime.today().time()
 	capture(sunTimes, still_interval)			# 1) get pics
 	stitch()						# 2) create timelapse
 	upload()						# 3) upload to YouTube
 	cleanup()						# 4) delete files
-	print "Done! at ",datetime.today().time().strftime("%H:%M"),"\n"	
+	print("Done! at ",datetime.today().time().strftime("%H:%M"),"\n")
 
 def runCommand(command):
 	shellCommand = command
@@ -53,10 +53,10 @@ def runCommand(command):
 	while True:
         	nextline = event.stdout.readline()
         	if nextline == '' and event.poll() != None:
-            		break
-		if nextline == '' or nextline.isspace():
-			break
-		print(nextline),
+        		break
+        		if nextline == '' or nextline.isspace():
+        			break
+        			print(nextline),
 
 # take pictures
 def capture(sunTimes, still_interval):
@@ -64,20 +64,20 @@ def capture(sunTimes, still_interval):
 	startTime = map(int, sunTimes[0].split(':'));
 	start_seconds = startTime[0] * 3600 + startTime[1] * 60	# start time in sec
 	endTime = map(int, sunTimes[1].split(':'));
-    end_seconds = endTime[0] * 3600 + endTime[1] * 60 		# end time in sec
-	print "Start time is: ",startTime
-	print "Start seconds is:",start_seconds
-	print "End time is: ",endTime
-	print "End seconds is:",end_seconds
+	end_seconds = endTime[0] * 3600 + endTime[1] * 60       # end time in sec
+	print("Start time is: ",startTime)
+	print("Start seconds is:",start_seconds)
+	print("End time is: ",endTime)
+	print("End seconds is:",end_seconds)
 	total_time = (end_seconds - start_seconds) * 1000 	# total time in ms
 	total_time_min = total_time/float(60000)                # total time in min
-	print "Total time is: ",total_time
+	print("Total time is: ",total_time)
 	still_interval_sec = still_interval/float(1000)		# still interval in sec
 	now = datetime.today().time().strftime("%H:%M")
-	print '\n',"==> Step 1 of 4: Taking stills..."
-	print "Starting at " + str(now) + ", \
+	print('\n',"==> Step 1 of 4: Taking stills...")
+	print("Starting at " + str(now) + ", \
         capturing still every " + str(still_interval_sec) + "s \
-        for " + str(total_time_min) + "min."
+        for " + str(total_time_min) + "min.")
 	testStills = "raspistill \
         -o image_%04d.jpg \
         -tl " + str(still_interval) + " \
@@ -94,7 +94,7 @@ def stitch():
         -vf scale=1920:1080 \
         -o tlcam.avi \
         -mf type=jpeg:fps=24 mf://@stills.txt"
-	print '\n',"==> Step 2 of 4: Stitching frames together..."
+	print('\n',"==> Step 2 of 4: Stitching frames together...")
 	runCommand(getStills)
 	runCommand(makeVideo)
 
@@ -106,23 +106,24 @@ def upload():
         -p <password> \
         -t 'Charlottesville Sunrise - " + date + "' -c 'Entertainment' \
         tlcam.avi"
-	print '\n',"==> Step 3 of 4: Uploading video..."
+	print('\n',"==> Step 3 of 4: Uploading video...")
 	runCommand(uploadVideo)
 
 # delete files
 def cleanup():
 	removeFiles = "rm *.jpg; rm tlcam.avi"
-	print '\n',"==> Step 4 of 4: Removing files..."
+	print('\n',"==> Step 4 of 4: Removing files...")
 	runCommand(removeFiles)
 
 # main loop
-print "==================================="
-print "========== ",datetime.today().strftime("%d %b %Y")," =========="
-print "==================================="
-times = getSunrise()
-still_interval = 1000	# still interval in ms
-print "Sunrise times: ",times
-wait_start(times, still_interval)
+getSunrise()
+# print "==================================="
+# print "========== ",datetime.today().strftime("%d %b %Y")," =========="
+# print "==================================="
+# times = getSunrise()
+# still_interval = 1000	# still interval in ms
+# print "Sunrise times: ",times
+# wait_start(times, still_interval)
 
 # testing
 #testTimes = times
