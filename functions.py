556708efce3960c8776d8f8efb023bac3b781b5f
Caleb Gross <caleb.gross@gmail.com>
2015-12-27 01:27:34 -1000

    Add testing environment variable, reformat times and strings

--- functions.py
+++ functions.py
@@ -9,6 +9,22 @@ from subprocess import Popen, PIPE, STDOUT
 from datetime import datetime
 from time import sleep
 
+def run_command(command):
+	print('Executing command:',command)
+	# if not testing:
+	# 	print('not testing')
+	# else:
+	# 	print('testing')
+		# event = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+		# poll process for new output until finished
+		# while True:
+		# 	nextline = event.stdout.readline()
+		# 	if nextline == '' and event.poll() != None:
+		# 		break
+		# 	if nextline == '' or nextline.isspace():
+		# 		break
+		# 		print(nextline),
+
 # get times for dawn and sunshine from the web
 def get_event_times():
 	url = 'http://www.gaisma.com/en/location/kailua-hawaii.html'
@@ -25,81 +41,71 @@ def get_event_times():
 
 # wait until dawn
 def wait_until_dawn(dawn):
-	if datetime.now() <= dawn:
-		print('were good')
+	print('Currently',datetime.now().time().strftime('%H:%M')+',', end=' ')
+	if datetime.now() >= dawn:
+		print('sun has started rising. Starting timelapse.')
 		return
 	else:
-		# print('Currently',datetime.strftime('%H:%M', datetime.now()+',',
-		# 	  'waiting until',datetime.strftime('%H:%M', dawn))
 		time_delta = dawn - datetime.now()
-		seconds_until_dawn = time_delta.seconds
-		print(seconds_until_dawn)
-		sleep(seconds_until_dawn+5)
-
-def run_command(command):
-	event = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-	# poll process for new output until finished
-	while True:
-		nextline = event.stdout.readline()
-		if nextline == '' and event.poll() != None:
-			break
-		if nextline == '' or nextline.isspace():
-			break
-			print(nextline),
+		seconds_until_dawn = time_delta.seconds + 5
+		print('sleeping',str(seconds_until_dawn),
+			  'seconds until',dawn.strftime('%H:%M'+'.'))
+		sleep(seconds_until_dawn)
+		wait_until_dawn(dawn)
 
 # take pictures
-def capture(sunTimes, capture_interval):
-	# take a picture every [30] seconds for [60] minutes
-	start_time = map(int, sunTimes[0].split(':'));
-	start_seconds = start_time[0] * 3600 + start_time[1] * 60	# start time in sec
-	end_time = map(int, sunTimes[1].split(':'));
-	end_seconds = end_time[0] * 3600 + end_time[1] * 60       # end time in sec
-	print("Start time is: ",start_time)
-	print("Start seconds is:",start_seconds)
-	print("End time is: ",end_time)
-	print("End seconds is:",end_seconds)
-	total_time = (end_seconds - start_seconds) * 1000 	# total time in ms
-	total_time_min = total_time/float(60000)                # total time in min
-	print("Total time is: ",total_time)
-	capture_interval_sec = capture_interval/float(1000)		# still interval in sec
-	now = datetime.today().time().strftime("%H:%M")
-	print('\n',"==> Step 1 of 4: Taking stills...")
-	print("Starting at " + str(now) + ", \
-		capturing still every " + str(capture_interval_sec) + "s \
-		for " + str(total_time_min) + "min.")
-	test_stills = "raspistill \
-	-o image_%04d.jpg \
-	-tl " + str(capture_interval) + " \
-	-t " + str(total_time)
-	run_command(test_stills)
+def capture(event_times, still_interval):
+	capture_interval = event_times['sunshine'] - event_times['dawn']
+	print('\n==> Step 1 of 4: Capturing stills...')
+	print('Starting at ' + event_times['dawn'].strftime('%H:%M') + 
+		', capturing stills every ' +
+		str(int(still_interval/1000))+ ' seconds for ' +
+		str(int(capture_interval.seconds/60)) + ' minutes.')
+	capture = (
+		'raspistill ' +
+		'-o still_%04d.jpg ' +
+		'-tl ' + str(still_interval) + ' ' +
+		'-t ' + str(capture_interval.seconds*1000)
+		)
+	run_command(capture)
+	return
 
 # compile video from frames
 def stitch():
-	get_stills = "ls *.jpg > stills.txt"
-	make_video = "mencoder \
-	-nosound \
-	-ovc lavc \
-	-lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 \
-	-vf scale=1920:1080 \
-	-o tlcam.avi \
-	-mf type=jpeg:fps=24 mf://@stills.txt"
-	print('\n',"==> Step 2 of 4: Stitching frames together...")
+	get_stills = 'ls still_*.jpg > stills_list.txt'
+	video_name = 'sunpise_' + datetime.now().strftime('%m-%d-%y_%H-%M') + '.avi'
+	make_video = (
+		'mencoder ' +
+		'-nosound ' +
+		'-ovc lavc ' +
+		'-lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 ' +
+		'-vf scale=1920:1080 ' +
+		'-o ' + video_name + 
+		'-mf type=jpeg:fps=24 mf://@stills_list.txt'
+		)
+	print('\n==> Step 2 of 4: Stitching frames together...')
 	run_command(get_stills)
 	run_command(make_video)
+	return video_name
 
-# upload to youtube
-def upload():
-	date = datetime.today().strftime("%d %b %Y")
-	upload = "/usr/local/bin/youtube-upload \
-	-m <email> \
-	-p <password> \
-	-t 'Kailua Sunrise - " + date + "' -c 'Entertainment' \
-	tlcam.avi"
-	print('\n',"==> Step 3 of 4: Uploading video...")
+# upload to YouTube
+def upload(video_name):
+	upload = (
+		'/usr/local/bin/youtube-upload ' + 
+		'-m <email> ' +
+		'-p <password> ' +
+		'-t "Kailua Sunrise - ' + datetime.now().strftime('%d %b %Y') + '" ' +
+		'-c Entertainment ' + 
+		video_name
+		)
+	print('\n==> Step 3 of 4: Uploading video...')
 	run_command(upload)
+	return
 
 # delete files
 def cleanup():
-	cleanup = "rm *.jpg; rm tlcam.avi"
-	print('\n',"==> Step 4 of 4: Removing files...")
+	cleanup = 'rm *.jpg; rm *.avi'
+	print('\n==> Step 4 of 4: Removing files...')
 	run_command(cleanup)
+	return
+
--- sunpise.py
+++ sunpise.py
@@ -20,26 +20,37 @@
 
 from functions import *
 
+testing        = True # development aid
+still_interval = 1000 # still interval in ms	
+
 def main():
+	if testing:
+		print('Testing!')
+		event_times = {
+		'dawn': datetime.now().replace(hour=0, minute=45, microsecond=0),
+		'sunshine': datetime.now().replace(hour=7, minute=5, microsecond=0)
+		}
+	else:
+		event_times = get_event_times()          # 1) get sunrise times
+
 	print('=================================')
 	print('==========',datetime.now().strftime('%d %b %Y'),'==========')
 	print('=================================')
-	capture_interval = 1000	                 # still interval in ms
-	event_times = get_event_times()          # 1) get sunrise times
+	
 	for key in event_times:
 		print(key + ':',event_times[key])
-	print("now:",datetime.now())
+	print("now:",datetime.now().replace(microsecond=0))
 	wait_until_dawn(event_times['dawn'])     # 2) wait until sunrise
-	# capture(event_times, capture_interval) # 3) get pics
-	# stitch()                               # 4) create timelapse
-	# upload()                               # 5) upload to YouTube
-	# cleanup()                              # 6) delete files
+	capture(event_times, still_interval) # 3) get pics
+	video_name = stitch()                               # 4) create timelapse
+	upload(video_name)                               # 5) upload to YouTube
+	cleanup()                              # 6) delete files
 	# print('Done at',datetime.strftime('%H:%M', localtime()),'\n')
 
 	# testing
 	# test_times = {'dawn': u'18:23', 'sunshine': u'19:23'}
 	# print(test_times)
-	# wait_start(test_times, capture_interval)
+	# wait_start(test_times, still_interval)
 
 if __name__ == "__main__":
     main()
