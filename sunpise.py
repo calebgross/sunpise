edb2788ba1719f5e70f7be49f265a6320778a232
Caleb Gross <c@leb.email>
2017-02-19 13:59:31 -1000

    Refactor code to be more pythonic

--- sunpise.py
+++ sunpise.py
@@ -60,129 +60,126 @@ def main():
     print_header()
 
     # 1) get event times
-    if not args['start_now']:
-        event_times = get_event_times()
-    else:
-        now = datetime.now()
-        event_times = {
-            'start': now,
-            'end': now + timedelta(0, int(args['capture_interval']))
-            }
+    event_times = get_event_times()
     print_times(event_times)
     
     # 2) wait until dawn to start timelapse
-    wait_until(event_times['start'])
+    wait_start(event_times['start'])
     
     # 3) start capturing stills
     capture(event_times)
     
     # 4) make video
-    video_name = stitch()            
+    video_name = stitch()
 
     # 5) upload video
-    upload(video_name)             
+    upload(video_name)
 
-    # 6) remove files from device      
-    cleanup()                  
+    # 6) remove files from device
+    cleanup()
 
-    print('\n==> Finished at',datetime.now().strftime('%H:%M')+'.\n')
-
-def run_command(command):
+def run_command(command, log=True):
 
     # log command
     printed_command = command
     prefix = 'Executing command: '
-    wrapper = textwrap.TextWrapper(initial_indent=prefix,
+    wrapper = textwrap.TextWrapper(
+        initial_indent=prefix,
         width=80, subsequent_indent=' '*int((len(prefix)/4)))
-    print(wrapper.fill(printed_command))
+    if log:
+        print(wrapper.fill(printed_command))
 
     # execute command
     if not args['debug']:
         os.system(command)
 
+# log event type, date, and debug status
 def print_header():
+    
+    # prepare header
     title_char = '~'
-    title = (args['location'].capitalize() + ' ' +
-        args['event_type'].capitalize()  + ' ' +
-        '-'*(len(args['event_type'])%2+2) + ' ' + 
-        datetime.now().strftime('%d %b %Y'))
+    title = (args['location'].title() + ' ' +
+             args['event_type'].capitalize()  + ' ' +
+             '-'*(len(args['event_type'])%2+2) + ' ' + 
+             datetime.now().strftime('%d %b %Y'))
     title_margin = int(40 - float(len(title) + 2)/2)
+    
+    # print header
     print(title_char * (2 * title_margin + 2 + len(title)))
     print(title_char * title_margin, title, title_char * title_margin)
     if args['debug']:
         print(title_char * (2 * title_margin + 2 + len(title)))
-        print(title_char * 34, 'DEBUG MODE', title_char * 34)
-    print(title_char * (2 * title_margin + 2 + len(title)), end='\n\n')
+        print(title_char * 36, 'DEBUG', title_char * 36)
+    print(title_char * (2 * title_margin + 2 + len(title)), end='\n\n')    
 
+# pretty print event times
 def print_times(event_times):
     for key in reversed(sorted(event_times.keys())):
-        print(key.capitalize() + ':' + ' ' *(9-len(key)) +
+        print(args['event_type'].capitalize(), key + ':' + ' ' * (9-len(key)) +
             event_times[key].strftime('%H:%M'))
 
 # get times for dawn and sunshine from the web
 def get_event_times():
 
-   # get sunrise/sunset info from API, loaded into JSON for easy parsing
+    if args['start_now']:
+        return {'start': datetime.now().replace(tzinfo=tzlocal()),
+                'end'  : datetime.now().replace(tzinfo=tzlocal()) +
+                         timedelta(0, int(args['capture_interval']))}
+
+   # query API for sunrise/sunset info, and load into JSON for easy parsing
     payload  = {'lat': coordinates[0], 'lng': coordinates[1], 'date': 'today'}
     url      = 'http://api.sunrise-sunset.org/json'
     response = json.loads(requests.get(url, params=payload).text)['results']
     
-    # initialize data structures to iterate through response
-    event_times = {}
+    # initialize data structures to iterate through JSON response
     today       = datetime.now()
-    event_names = {
-        'sunrise': {
-            'start': 'civil_twilight_begin',
-            'end':   'sunrise'
-        },
-        'sunset' : {
-            'start': 'sunset',
-            'end':   'civil_twilight_end'
-        }    
-    }
+    event_names = {'sunrise': {'start': 'civil_twilight_begin',
+                               'end'  : 'sunrise'},
+                   'sunset' : {'start': 'sunset',
+                               'end'  : 'civil_twilight_end'}}
 
-    # for events start and end
-    for event_name in sorted(event_names[args['event_type']].keys()):
-        
-        # 1) create datetime object from API response string
-        # 2) make the datetime timezone-aware (API returns in UTC)
-        # 3) convert to local timezone
-        # 4) complete the datetime with local year, month, and day
-        event_times[event_name] = datetime.strptime(
-            response[event_names[args['event_type']][event_name]],
-            '%I:%M:%S %p'
-            ).replace(
-            year=today.year, tzinfo=UTC
-            ).astimezone(tzlocal()).replace(
-            year=today.year, month=today.month, day=today.day)
-    
+    # for events "start" and "end":
+    #   1) create datetime object from API response string
+    #   2) make the datetime timezone-aware (API returns in UTC)
+    #   3) convert to local timezone
+    #   4) complete the datetime with local year, month, and day
+    event_times = {event_name: datetime.strptime(
+        response[event_names[args['event_type']][event_name]],
+        '%I:%M:%S %p').replace(
+        year=today.year, tzinfo=UTC).astimezone(tzlocal()).replace(
+        year=today.year, month=today.month, day=today.day)
+        for event_name in sorted(event_names[args['event_type']].keys())}
+
     return event_times
 
-# wait until dawn
-def wait_until(start):
+# wait until event start
+def wait_start(start):
 
+    # print current time
     print('Currently', datetime.now().time().strftime('%H:%M')+',', end=' ')
 
-    # check if sun is rising/setting
+    # check if event has started
     if datetime.now().replace(tzinfo=tzlocal()) >= start:
         print('sun has started ' +
-            ('rising' if args['event_type'] == 'sunrise' else 'setting') +
-            '. Starting timelapse.')
+              ('rising' if args['event_type'] == 'sunrise' else 'setting') +
+              '. Starting timelapse.')
         return
-
-    # wait until start and check again
     else:
+        
+        # determine how long to wait until event starts
         time_delta = start - datetime.now().replace(tzinfo=tzlocal())
         seconds_until_start = time_delta.seconds + 5
-        if seconds_until_start > 3600:
-            sleep_time = str(round(seconds_until_start/3600, 2)) + ' hours until'
-        elif seconds_until_start > 60:
-            sleep_time = str(round(seconds_until_start/60, 2)) + ' minutes until'
+        if seconds_until_start >= 3600:
+            sleep_time = str(round(seconds_until_start/3600, 2)) + ' hours'
+        elif seconds_until_start >= 60:
+            sleep_time = str(round(seconds_until_start/60, 2)) + ' minutes'
         else:
-            sleep_time = str(seconds_until_start) + ' seconds until'
-        print('sleeping',sleep_time,start.strftime('%H:%M'+'.'))
+            sleep_time = str(seconds_until_start) + ' seconds'
+
+        # wait until calculated time
+        print('sleeping',sleep_time,'until',start.strftime('%H:%M')+'.')
         sleep(seconds_until_start)
-        wait_until(start)
+        wait_start(start)
 
 # take pictures
 def capture(event_times):
@@ -190,13 +187,12 @@ def capture(event_times):
         capture_interval = (event_times['end'] - event_times['start']).seconds
     else:
         capture_interval = args['capture_interval']
-    capture = (
-        'raspistill ' +
-        '--burst ' + 
-        '-o ' + args['directory'] + 'stills/still_%04d.jpg ' +
-        '-tl ' + str(args['still_interval']) + ' ' +
-        '-t ' + str(capture_interval*1000)
-        )
+    stills_dir = args['directory'] + 'stills/'
+    capture = ('raspistill ' +
+               '--burst ' + 
+               '-o ' + stills_dir + 'still_%04d.jpg ' +
+               '-tl ' + str(args['still_interval']) + ' ' +
+               '-t ' + str(capture_interval*1000))
     if args['upside_down']:
         capture += ' --hflip --vflip'
     print('\n==> Step 1 of 4 (' +
@@ -208,32 +204,24 @@ def capture(event_times):
     run_command(capture)  
     
     # account for unprocessed stills so avconv can process input
-    if not args['debug']:
-        filenames = os.listdir('/home/pi/sunpise/stills')
-        change_filename = lambda filename, new_filename: ('mv ' +
-            '/home/pi/sunpise/stills/' + filename +
-            ' ' +
-            '/home/pi/sunpise/stills/' + new_filename
-            )
-        i = 0
-        for filename in sorted(filenames):
-            new_filename = 'still_' + str(int(i)).zfill(4) + '.jpg'
-            os.system(change_filename(filename, new_filename))
-            i += 1
+    filenames = os.listdir(stills_dir)
+    for i, filename in enumerate(sorted(filenames)):
+        new_filename = 'still_' + str(int(i)).zfill(4) + '.jpg'
+        run_command(
+            'mv ' + stills_dir + filename + ' ' + stills_dir + new_filename,
+            False)
 
     return
 
 # compile video from frames
 def stitch():
     video_name = 'sunpise' + datetime.now().strftime('_%m-%d-%y_%H-%M') + '.avi'
-    make_video = (
-        'avconv ' +
-        '-f image2 ' + 
-        '-i ' + args['directory'] + 'stills/still_%04d.jpg ' + 
-        '-r 12 ' + 
-        '-s 1920x1080 ' + 
-        args['directory'] + video_name
-        )
+    make_video = ('avconv ' +
+                  '-f image2 ' + 
+                  '-i ' + args['directory'] + 'stills/still_%04d.jpg ' + 
+                  '-r 12 ' + 
+                  '-s 1920x1080 ' + 
+                  args['directory'] + video_name)
     print('\n==> Step 2 of 4 (' +
         datetime.now().strftime('%H:%M') + '): Stitching frames together...')
     run_command(make_video)
@@ -327,7 +315,7 @@ def resumable_upload(insert_request):
 def upload(video_name):
 
     # prepare arguments to YouTube API
-    location_formatted = re.sub(r'-.*', '', args['location']).capitalize()
+    location_formatted = re.sub(r'-.*', '', args['location']).title()
     event_type_formatted = args['event_type'].capitalize()
     yt_args = argparse.Namespace(
         auth_host_name='localhost', 
@@ -366,6 +354,7 @@ def cleanup():
     print('\n==> Step 4 of 4 (' +
         datetime.now().strftime('%H:%M') + '): Removing files...')
     run_command(cleanup)
+    print('\n==> Finished at',datetime.now().strftime('%H:%M')+'.\n')
     return
 
 if __name__ == "__main__":
